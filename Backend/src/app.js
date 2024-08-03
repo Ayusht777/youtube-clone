568b1b5b53e143c73358cac4173bc73f7f445e61
Ayusht777 <talesarayush@gmail.com>
2024-08-03 18:44:35 +0530

    api routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -13,5 +13,12 @@ app.use(express.urlencoded({ extended: true })); // it use for url handling  / r
 app.use(express.static("public")); // it is use to store files like data from pdf or images etc
 
 app.use(cookieParser()); // use for cookies handling on server level
+
+//Routes
+import userRoutes from "./routes/user.routes.js";
+//Routes Declarations
+app.use("/api/v1/users", userRoutes);
+
+//http:localhost:8080//api/v1/users/register
 
 export { app };
