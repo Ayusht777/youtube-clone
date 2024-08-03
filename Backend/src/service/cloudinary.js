20c9cbd9380e12df74c46d6fd194af8d1ee1218d
Ayusht777 <talesarayush@gmail.com>
2024-08-03 08:51:04 +0530

    cloudnairy & multer

---
+++ Backend/src/service/cloudinary.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const uploadFileCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null; //no file path then return null
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //file uploaded successfully
+    console.log(`File is Uploaded on Cloudnairy -> ${response}`);
+    return response; // idealy should retun url
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // reomve file sync way in tempoary storage of server as the upload got failed
+    return null;
+  }
+};
+
+export {uploadFileCloudinary}
