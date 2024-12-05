9f5b4755e7ba469247af390e78dd822be44e5414
ayush <ayush.talesara777.appcrave@gmail.com>
2024-12-05 17:03:06 +0530

    completed dashboard.controller.js

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
-import { Subscription } from "../models/subscription.model.js";
-import { Like } from "../models/like.model.js";
+import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -17,10 +16,67 @@ const getChannelStats = asyncHandler(async (req, res) => {
   }
 
   const getChannelStats = await User.aggregate([
-    { $match:{ _id: new mongoose.Types.ObjectId(userId)}},
+    { $match: { _id: new mongoose.Types.ObjectId(userId) } },
+    {
+      $project: { _id: 1 },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "_id",
+        foreignField: "owner",
+        pipeline: [{ $project: { views: 1, isPublished: 1 } }],
+        as: "videos",
+      },
+    },
+    {
+      $lookup: {
+        from: "comments",
+        localField: "videos._id",
+        foreignField: "video",
+        pipeline: [{ $project: { _id: 1 } }],
+        as: "comments",
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "videos._id",
+        foreignField: "videoId",
+        pipeline: [{ $project: { _id: 1 } }],
+        as: "likes",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channelId",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+            },
+          },
+        ],
+        as: "subscibers",
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        totalViews: { $sum: "$videos.views" },
+        totalComments: { $size: "$comments" },
+        totalLikes: { $size: "$likes" },
+        totalVideos: { $size: "$videos" },
+        totalSubscribers: { $size: "$subscibers" },
+      },
+    },
   ]);
 
-  return res.status(200).json(new ApiResponse(200, 0, "Channel Stats"));
+  return res
+    .status(200)
+    .json(new ApiResponse(200, getChannelStats, "Channel Stats"));
 });
 
 const getChannelVideos = asyncHandler(async (req, res) => {
@@ -28,15 +84,21 @@ const getChannelVideos = asyncHandler(async (req, res) => {
   // get user id then get all videos uploaded by that user
   //get info like title, description, views, likes,comments, thumbnail, publishedAt,
   const userId = req.user.id;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, query } = req.query;
   const pageNumber = Math.max(1, parseInt(page));
   const pageLimit = Math.max(1, Math.min(parseInt(limit), 20)); //max pageLimit is 20
+  console.log(query);
   if (!userId) {
     throw new ApiError(400, "User not found");
   }
 
   const getChannelVideosPipeline = [
-    { $match: { owner: new mongoose.Types.ObjectId(userId) } },
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+        ...(query && { $text: { $search: query } }),
+      },
+    },
     { $sort: { createdAt: -1 } },
     {
       $lookup: {
