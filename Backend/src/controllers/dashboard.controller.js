f1afbbbb9f40f894d15969de3c13f68e21fd9f91
ayush <ayush.talesara777.appcrave@gmail.com>
2024-12-11 19:23:04 +0530

    Update dashboard.controller.js

--- Backend/src/controllers/dashboard.controller.js
+++ Backend/src/controllers/dashboard.controller.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
+import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -96,7 +96,13 @@ const getChannelVideos = asyncHandler(async (req, res) => {
     {
       $match: {
         owner: new mongoose.Types.ObjectId(userId),
-        ...(query && { $text: { $search: query } }),
+        ...(query && {
+          $text: {
+            $search: query,
+            $caseSensitive: false,
+            $diacriticSensitive: false,
+          }, // IT WILL WORK "Cafe Tutorial" "CAFÉ TUTORIA" "Cafè Tutorial""cafe TUTORIAL"
+        }),
       },
     },
     { $sort: { createdAt: -1 } },
