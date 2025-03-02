a6173705b6503fe2d26220365100f0e4f4ef66fd
Ayusht777 <talesaraayush@gmail.com>
2025-03-02 11:19:31 +0530

    Remove frontend project files and configurations

--- Frontend/src/store/useLayoutStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useLayoutStore = create((set) => ({
-  isSidebarOpen: false,
-  toggleSidebar: () => set((state) => ({ isSidebarOpen: !state.isSidebarOpen })),
-}));
-
-export default useLayoutStore;
