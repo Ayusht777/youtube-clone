7bd3eca2fb99c4cc99be11ef408f36d52dea3c82
Ayusht777 <talesaraayush@gmail.com>
2025-03-13 22:31:20 +0530

    Add Axios instance configuration for API requests

---
+++ Frontend/src/lib/axios.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from "axios";
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+
+export default axiosInstance;
