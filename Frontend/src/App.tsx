efc7e17688900378f1695a0e8a8e414e01911eef
Ayusht777 <talesaraayush@gmail.com>
2025-03-14 15:12:16 +0530

    Add login page and route to App component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { RootLayout } from "@/layouts/RootLayout";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import LoginPage from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route element={<RootLayout />}>
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
