d1523b73155361c8ebcef113607de82038bd2978
Ayusht777 <talesaraayush@gmail.com>
2025-03-15 23:50:22 +0530

    Update ProfilePage layout for improved styling:
    - Simplify the container class for the ProfilePage by removing unnecessary padding and max-width constraints.

--- Frontend/src/pages/profile-page.tsx
+++ Frontend/src/pages/profile-page.tsx
@@ -2,7 +2,7 @@ import UserSettingTab from "@/components/profile/user-setting-tab";
 
 export default function ProfilePage() {
   return (
-    <div className="container mx-auto py-6 max-w-5xl">
+    <div className=" mx-auto ">
       <UserSettingTab />
     </div>
   );
